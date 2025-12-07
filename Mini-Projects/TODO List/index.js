896393eb149a1520ea77a4744c35d48f794e707c
shekhar3742 <imderek3742@gmail.com>
2025-12-07 09:16:04 +0530

    Add Delete checked button

--- Mini-Projects/TODO List/index.js
+++ Mini-Projects/TODO List/index.js
@@ -1,21 +1,31 @@
 const add = () => {
     let Input = document.getElementById('input');
     let InputValue = Input.value.trim();
-    
-    if(InputValue !== ""){
-       const List =  document.getElementById('List');
-       const li = document.createElement('li');
-       li.innerHTML = '<input type="checkbox"> '+InputValue + ' <Button onclick="deleteTask(this)" >Delete</Button>';
-       List.appendChild(li);
-       Input.value = ''
+
+    if (InputValue !== "") {
+        const List = document.getElementById('List');
+        const li = document.createElement('li');
+        li.innerHTML = '<input type="checkbox"> ' + InputValue + ' <Button onclick="deleteTask(this)" >Delete</Button>';
+        List.appendChild(li);
+        Input.value = ''
     }
-    else{
+    else {
         alert("Please add a task ");
     }
 
 }
 
-const deleteTask = (btn) =>{
-    const liparent  = btn.parentNode;
+const deleteTask = (btn) => {
+    const liparent = btn.parentNode;
     liparent.parentNode.removeChild(liparent);
+}
+
+const deleteChecked = () => {
+    let Input = document.getElementById('input');
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+
+    checkboxes.forEach(checkbox => {
+        const liparent = checkbox.parentNode;
+        liparent.parentNode.removeChild(liparent);
+    })
 }
