9370a96c4452f4f0fc928599cf774ed30b93691a
shekhar3742 <imderek3742@gmail.com>
2025-12-07 08:35:37 +0530

    Add Button

--- Mini-Projects/TODO List/index.js
+++ Mini-Projects/TODO List/index.js
@@ -1,25 +1,15 @@
-let Input = document.getElementById('input');
-let ul = document.getElementById('List');
-
-
 const add = () => {
-    let div = document.createElement('div');
-    let li = document.createElement('li');
-    let button = document.createElement('button');
-    button.textContent = "Delete";
-    button.style.margin = "0px 0px 0px 10px"
-
-
-    div.style.display = "flex";
-    div.style.marginBottom = "10px";
-    let checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    let divElement = ul.appendChild(div);
-    let addcheckbox = divElement.appendChild(checkbox);
-    let addTask = divElement.appendChild(li);
-    let addButton = divElement.appendChild(button);
-
-
-    addTask.textContent = Input.value;
+    let Input = document.getElementById('input');
+    let InputValue = Input.value.trim();
+    
+    if(InputValue !== ""){
+       const List =  document.getElementById('List');
+       const li = document.createElement('li');
+       li.innerHTML = '<input type="checkbox"> '+InputValue + ' <Button>Delete</Button>';
+       List.appendChild(li);
+    }
+    else{
+        alert("Please add a task ");
+    }
 
 }
