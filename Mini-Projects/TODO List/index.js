4f94d8e65da2ec7d6f0868f3727855bb113867ba
shekhar3742 <imderek3742@gmail.com>
2025-12-07 08:57:21 +0530

    Add delete button

--- Mini-Projects/TODO List/index.js
+++ Mini-Projects/TODO List/index.js
@@ -5,11 +5,17 @@ const add = () => {
     if(InputValue !== ""){
        const List =  document.getElementById('List');
        const li = document.createElement('li');
-       li.innerHTML = '<input type="checkbox"> '+InputValue + ' <Button>Delete</Button>';
+       li.innerHTML = '<input type="checkbox"> '+InputValue + ' <Button onclick="deleteTask(this)" >Delete</Button>';
        List.appendChild(li);
+       Input.value = ''
     }
     else{
         alert("Please add a task ");
     }
 
+}
+
+const deleteTask = (btn) =>{
+    const liparent  = btn.parentNode;
+    liparent.parentNode.removeChild(liparent);
 }
